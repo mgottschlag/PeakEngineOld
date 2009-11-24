82a79efeb2be955ebc0917787b88d27f0738b2ae
Mathias Gottschlag <mathias-go@web.de>
2009-11-24 16:53:11 +0100

    Documented most of Entity.

--- include/peakengine/entity/Entity.hpp
+++ include/peakengine/entity/Entity.hpp
@@ -28,10 +28,36 @@ namespace peak
 	class Property;
 	class Buffer;
 
+	/**
+	 * Dynamic game object, this can be everything from the player to objects
+	 * like doors or a scoreboard. The user entity is not derived from this
+	 * class but rather from ServerEntity or ClientEntity, depending on where
+	 * the code is supposed to be run. See also EntityFactory and
+	 * Game::registerFactory() for information on how to register entity types
+	 * in the engine.
+	 *
+	 * Entities are synchronized over the network via Property, properties are
+	 * added via addProperty() and the server is in control over the values of
+	 * the properties.
+	 *
+	 * There also are Properties which are controlled by the client and can be
+	 * used for example input, use addClientProperty() for these (the properties
+	 * must be added only on the server and on the client which owns the
+	 * entity).
+	 */
 	class Entity
 	{
 		public:
+			/**
+			 * Constructor.
+			 * @param manager EntityManager into which this entity will get
+			 * inserted later. This does not insert the entity into the manager
+			 * yet!
+			 */
 			Entity(EntityManager *manager);
+			/**
+			 * Destructor.
+			 */
 			virtual ~Entity();
 
 			bool hasChanged(unsigned int time);
@@ -44,23 +70,74 @@ namespace peak
 			void applyClientUpdate(Buffer *buffer, unsigned int time);
 			void getClientUpdate(Buffer *buffer, unsigned int time);
 
+			/**
+			 * Saves a backup of all properties. This backup will be applied
+			 * before the next server update is applied. This has to be called
+			 * on the server before modifying the properties (e.g. for client
+			 * predition).
+			 */
 			void createBackup();
+			/**
+			 * Returns true if there has been a backup since the last server
+			 * update.
+			 */
 			bool hasBackup();
+			/**
+			 * Applies the backup. Usually only called by Client when a server
+			 * update arrives. The backup stays valid, discardBackup() is not
+			 * called by this function.
+			 */
 			void applyBackup();
+			/**
+			 * Drops the backup without applying it.
+			 */
 			void discardBackup();
 
+			/**
+			 * Returns the manager which contains this entity.
+			 */
 			EntityManager *getManager();
 
+			/**
+			 * Sets the ID of the entity. Must not be called by the user. IDs
+			 * are automatically created in Server.
+			 */
 			void setID(unsigned int id);
+			/**
+			 * Returns the ID of the entity.
+			 */
 			unsigned int getID();
 
+			/**
+			 * Callback which is called whenever the entity received an entity
+			 * message (via ServerEntity::sendMessage() or
+			 * ClientEntity::sendMessage()).
+			 */
 			virtual void receiveMessage(Buffer *buffer) = 0;
 
+			/**
+			 * Returns a string with the name of the entity (e.g. "player").
+			 * Must be implemented by the user and is used for entity
+			 * replication on the client.
+			 */
 			virtual std::string getType() = 0;
 
+			/**
+			 * Updates the entity. Can be implemented by the user.
+			 */
 			virtual void update();
 
+			/**
+			 * Adds a property to the property list. Properties have to be added
+			 * in the same order on both server and client! Also, there must
+			 * not be different counts or types of properties.
+			 */
 			void addProperty(Property *property);
+			/**
+			 * Adds a property to the client property list. This has to be
+			 * called for all client properties on the server and on the client
+			 * owning the entity.
+			 */
 			void addClientProperty(Property *property);
 		private:
 			std::vector<Property*> properties;
