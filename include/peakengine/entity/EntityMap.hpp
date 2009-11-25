da6428c9a8ce83ce5d52c61229296174b26d7fc0
Mathias Gottschlag <mathias-go@web.de>
2009-11-25 17:29:25 +0100

    Documented EntityMap.

--- include/peakengine/entity/EntityMap.hpp
+++ include/peakengine/entity/EntityMap.hpp
@@ -23,25 +23,85 @@ namespace peak
 {
 	class Entity;
 
+	/**
+	 * Custom container for entities, contains entity-id pairs in order to be
+	 * able to quickly retrieve a certain Entity. For iterating through the
+	 * entities, EntityMap::Iterator is used, example:
+	 * @code
+	 * // Create a map with some entities
+	 * EntityMap map;
+	 * int id0 = map.addEntity(entity0);
+	 * entity0->setID(id0);
+	 * map.addEntity(entity1);
+	 * entity1->setID(id1);
+	 * // Loop through all entities
+	 * EntityMap::Iterator it(map);
+	 * Entity *next = it.next();
+	 * while (next)
+	 * {
+	 *     std::cout << "Entity: " << next->getID() << std::endl;
+	 *     next = it.next();
+	 * }
+	 * @endcode
+	 */
 	class EntityMap
 	{
 		public:
+			/**
+			 * Constructor.
+			 */
 			EntityMap();
+			/**
+			 * Destructor.
+			 */
 			~EntityMap();
 
+			/**
+			 * Picks the next free ID and inserts the entity at this position.
+			 * Entity::setID() is not called, this has to be done by the user!
+			 * @return ID of the entity.
+			 */
 			unsigned int addEntity(Entity *entity);
+			/**
+			 * Adds an entity at a specific position, overwriting existing
+			 * entites with this id.
+			 */
 			void addEntity(Entity *entity, unsigned int id);
+			/**
+			 * Removes the entity with the given ID from the container.
+			 */
 			void removeEntity(unsigned int id);
+			/**
+			 * Retrieves the entity with the given ID, returns 0 if this ID is
+			 * currently unused.
+			 */
 			Entity *getEntity(unsigned int id);
 
+			/**
+			 * Returns the number of entities in the container.
+			 */
 			unsigned int getEntityCount();
 
+			/**
+			 * Class to iterate through the content of an EntityMap. If the map
+			 * is changed after the iterator has been initialized, entities
+			 * might be missed.
+			 */
 			class Iterator
 			{
 				public:
+					/**
+					 * Constructor.
+					 */
 					Iterator(EntityMap &map);
+					/**
+					 * Destructor.
+					 */
 					~Iterator();
 
+					/**
+					 * Returns the pointer to the next entity.
+					 */
 					Entity *next();
 				private:
 					EntityMap &map;
